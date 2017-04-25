41c4daef7d4b51e4588852ab21286edc0d5dc6b3
fenea <fenea95@gmail.com>
2017-04-25 10:52:31 +0300

    first online look

---
+++ assets/js/index.js
@@ -0,0 +1,15 @@
+function initialize(){
+	var username = "fenea95";
+	var first_name = "john";
+	var last_name = 'smith';
+	var email = 'fenea@gmail.com';
+	var password = '************';
+	// document.getElementById('name').innerHTML = first_name + " " + last_name;
+	$(".name").html(first_name + " " + last_name);
+	$(".usern").html(username);
+	$(".last-name").html(first_name);
+	$(".first-name").html(last_name);
+	$(".email").html(email);
+	$(".passw").html(password);
+	
+}
